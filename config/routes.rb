f6f2f6970c3e7f173c848976ca2fc36ab9b527eb
Mikhail Zaitsau <mikhail_zaitsau@outlook.com>
2023-01-06 00:48:59 +0300

    Generate devise User model

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,7 @@
 Rails.application.routes.draw do
+  devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   root to: "home#index"
-  Rails.application.routes.draw do
-    devise_for :users, controllers: {
-      sessions: 'users/sessions'
-    }
-  end
-  devise_for :users, path: 'auth', path_names: { sign_in: 'login',
-          sign_out: 'logout', password: 'secret', confirmation: 'verification',
-          unlock: 'unblock', registration: 'register', sign_up: 'cmon_let_me_in'
-        }
-  # Defines the root path route ("/")
+    # Defines the root path route ("/")
   # root "articles#index"
 end
